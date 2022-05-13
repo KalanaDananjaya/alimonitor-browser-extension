7327ad1e1c1d6e40e91d0330e8fe608bfe774aba
Kalana <kwijethu@cern.ch>
2022-05-13 17:02:28 +0200

    Fix menu tree

--- src/RecursiveTreeView.tsx
+++ src/RecursiveTreeView.tsx
@@ -12,6 +12,8 @@ import { Typography } from '@material-ui/core';
 function RecursiveTreeView(props) {
 
 const [menu, setMenu] = useState(JsonMenu);
+// const [menuUrls, setMenuUrls] = useState({});
+const menuUrls = {}
 
 const useStyles = makeStyles({
     root: {
@@ -21,23 +23,86 @@ const useStyles = makeStyles({
     },
 });
 
-const handleTreeItemClick = (ele, node) => {
+
+
+const updatedMenu = (menu) => {
+    console.log(menu, menu.children, menu.children.length)
+    if (menu.children.length > 0) {
+        
+        // console.log('actual menu', menu)
+        console.log('menu url is', menu.url, menu.url !== "", menu.url !==undefined)
+        if (menu.url !== undefined && menu.url !== null && menu.url !== "") {
+            const clonedChild = {
+                id : menu.id + "X",
+                name : menu.name,
+                url : menu.url,
+                children : []
+            }
+            menu.url = '';
+            menu.children.push(clonedChild)
+            console.log('pushed', clonedChild, 'as child of ', menu)
+            // add child
+            console.log('settign to null', menu.id)
+            
+        }
+    }
+    
+    var test = menuUrls;
+    menuUrls[menu.id] = menu.url;
+    // setMenuUrls(menuUrls)
+    // console.log('menuUrls',menuUrls)
+
+    if (menu.children.length > 0) {
+        // console.log('children exists', menu.children)
+        for (let index in menu.children) {
+            let child = menu.children[index]
+            // console.log('iterating childs', child.id, index, child)
+            updatedMenu(child)
+        }
+        // console.log("finished iterating children of", menu.id);
+    } else {
+        return
+    }
+}
+useEffect(() => {
+
+    updatedMenu(menu)
+    setMenu(menu)
+    console.log('update is', menu)
+    console.log('final menuUrls is', menuUrls['0'], Object.keys(menuUrls), Object.entries(menuUrls))
+  }, []);
+
+
+// console.log('final menuUrls is', menuUrls)
+// setMenuUrls(menuUrls)
+// console.log('keys are', menuUrls.keys())
+
+
+const handleTreeItemClick = (event, nodeId) => {
+    // console.log(menu)
     // props.toggleMenu();
-    console.log('menu is', ele, node)
-    // var url = node.url;
+    console.log('clicked nodeId is', nodeId, menuUrls[nodeId], "-")
+    var url = menuUrls[nodeId];
+
+    if(menuUrls.hasOwnProperty(nodeId)){
+        console.log("EXITS")
+    } else {
+        console.log("NOT EXISTS")
+    }
     // if (node.children.length === 0) {
-    //     if (url === null) {
-    //         // do nothing
-    //         // chrome.tabs.create({ url: newURL });
-    //     } 
-    //     // If the url starts with http, do not add the prefix
-    //     else if (/^(http)/.test(url)){
-    //         // chrome.tabs.create({ url: newURL });
-    //         window.open(url, "_blank");
-    //     } else {
-    //         // chrome.tabs.create({ url: newURL });
-    //         window.open(menu.prefix + url, "_blank");
-    //     }
+        console.log('url is ', url)
+        if (url === null || url ==='' || url === undefined) {
+            // do nothing
+            // chrome.tabs.create({ url: newURL });
+        } 
+        // If the url starts with http, do not add the prefix
+        else if (/^(http)/.test(url)){
+            // chrome.tabs.create({ url: newURL });
+            window.open(url, "_blank");
+        } else {
+            // chrome.tabs.create({ url: newURL });
+            window.open(menu.prefix + url, "_blank");
+        }
     // }
   
 }
@@ -45,35 +110,9 @@ const handleTreeItemClick = (ele, node) => {
 const classes = useStyles();
 
     const renderTree = (node) => {
-        // // // console.log(node.children.length)
-        // if (node.children.length > 0 && node.url !== null) {
-        //     // console.log('inside')
-        //     const newNode = {
-        //         id : node.id,
-        //         name : node.name,
-        //         url : node.url,
-        //         children: []
-        //     }
-        //     return (
-        //         <TreeItem key={node.id.toString()} nodeId={node.id.toString()} label={node.name}>
-        //             {/* <TreeItem key={newNode.id.toString()} nodeId={newNode.id.toString()} label={newNode.name}></TreeItem> */}
-        //             {Array.isArray(node.children) ? node.children.map((node) => renderTree(node)) : null}
-        //         </TreeItem>
-        //     ) 
-        // } else {
-        //     return (
-        //         <TreeItem key={node.id.toString()} nodeId={node.id.toString()} label={node.name}>
-        //             {Array.isArray(node.children) ? node.children.map((node) => renderTree(node)) : null}
-        //         </TreeItem>
-        //     )
-        // }
         return (
-            // <TreeItem key={node.id.toString()} nodeId={node.id.toString()} label={node.name} onLabelClick={() => handleTreeItemClick(node)}>
-            //         {Array.isArray(node.children) ? node.children.map((node) => renderTree(node)) : null}
-            //     </TreeItem>
             <TreeItem key={node.id.toString()} nodeId={node.id.toString()} label={node.name}>
                     {Array.isArray(node.children) ? node.children.map((node) => renderTree(node)) : null}
-                    {/* <Typography>hello</Typography> */}
                 </TreeItem>
         )
     };
